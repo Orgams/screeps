a9292ad9fcd2720f64a3943816900a37c178e78a
Moncho <morgan.moncho@gmail.com>
2018-12-16 20:50:09 +0100

    configure Janitor

--- role.janitor.js
+++ role.janitor.js
@@ -2,7 +2,7 @@ var bot = require('bot');
 
 var role = {
     run: function(creep) {
-        return bot.run(creep, ['janitor'], [FIND_DROPPED_RESOURCES]);
+        return bot.run(creep, ['transfer'], [FIND_DROPPED_RESOURCES, STRUCTURE_CONTAINER, STRUCTURE_STORAGE]);
     }
 }
 
