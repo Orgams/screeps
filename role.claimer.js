8a170e49817634d2d087dd1daad6fdea3f32a476
Moncho <morgan.moncho@gmail.com>
2018-12-21 12:51:37 +0100

    EVO : aller dans une salle adjacente au hasard
    aller dans une salle adjacente au hasard : récupérer une salle adjacente
    
    aller dans une salle adjacente au hasard : récupérer une salle adjacente
    
    aller dans une salle adjacente au hasard : trouver le chemin vers cette salle
    
    aller dans une salle adjacente au hasard : trouver le chemin vers cette salle
    
    aller dans une salle adjacente au hasard : trouver le chemin vers cette salle
    
    aller dans une salle adjacente au hasard : trouver le chemin vers cette salle
    
    aller dans une salle adjacente au hasard : trouver le chemin vers cette salle
    
    aller dans une salle adjacente au hasard : trouver le chemin vers cette salle
    
    aller dans une salle adjacente au hasard : trouver le chemin vers cette salle
    
    aller dans une salle adjacente au hasard : mémoriser le chemin

--- role.claimer.js
+++ role.claimer.js
@@ -1,12 +1,27 @@
 var bot = require('bot');
 
 var role = {
-    run: function(creep) {
-    	//console.log("claimer")
-        //return bot.run(creep, ['build', 'repair','transfer', 'storager']);
-        let rooms = Game.map.describeExits("W3N24");
+	run: function(creep) {
+		//console.log("claimer")
+		//return bot.run(creep, ['build', 'repair','transfer', 'storager']);
+		let rooms = Game.map.describeExits(creep.room.name);
 
-        if(false){
+		// Verifier si on peut controler plus de Salles
+		if(!Game.gcl.level > Object.keys(Game.rooms).length)return;
+
+		console.log(creep.memory.target)
+		if (creep.memory.target === undefined){
+			let keysRooms = Object.keys(rooms);
+			let keyRoom = keysRooms[Math.floor(Math.random() *keysRooms.length)];
+			let room = rooms[keyRoom];
+			let findExit = creep.room.findExitTo (room);
+			creep.memory.target = creep.pos.findClosestByRange (findExit); 
+			console.log(keysRooms, keyRoom, room, findExit, creep.memory.target);
+		}
+
+
+
+		if(false){
 			if(creep.room.controller && !creep.room.controller.my) {
 				if(creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(creep.room.controller);
@@ -25,7 +40,7 @@ var role = {
 				}
 			}
 		}
-    }
+	}
 }
 
 module.exports = role;
