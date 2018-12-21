c501274221530120af99943f22bcb8ca05614fa0
Moncho <morgan.moncho@gmail.com>
2018-12-21 13:26:41 +0100

    EVO : vérifier si la salle actuel est à moi
    EVO : vérifier si la salle actuel est à moi

--- role.claimer.js
+++ role.claimer.js
@@ -4,19 +4,19 @@ var role = {
 	run: function(creep) {
 		//console.log("claimer")
 		//return bot.run(creep, ['build', 'repair','transfer', 'storager']);
-		let rooms = Game.map.describeExits(creep.room.name);
+
+		console.log(creep.room.controller.my)
 
 		// Verifier si on peut controler plus de Salles
 		if(!Game.gcl.level > Object.keys(Game.rooms).length)return;
 
-		console.log(creep.memory.target)
+		let rooms = Game.map.describeExits(creep.room.name);
 		if (creep.memory.target === undefined){
 			let keysRooms = Object.keys(rooms);
 			let keyRoom = keysRooms[Math.floor(Math.random() *keysRooms.length)];
 			let room = rooms[keyRoom];
 			let findExit = creep.room.findExitTo (room);
-			creep.memory.target = creep.pos.findClosestByRange (findExit); 
-			console.log(keysRooms, keyRoom, room, findExit, creep.memory.target);
+			creep.memory.target = creep.pos.findClosestByRange (findExit);
 		}
 
 
