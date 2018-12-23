23659e7f06bf0e5ea93cb5063d7128bc470817cc
Moncho <morgan.moncho@gmail.com>
2018-12-23 08:07:32 +0100

    FIX ; delete console.log

--- role.claimer.js
+++ role.claimer.js
@@ -17,10 +17,8 @@ let role = {
 				let room = rooms[keyRoom];
 				let findExit = creep.room.findExitTo (room);
 				creep.memory.target = creep.pos.findClosestByRange (findExit);
-				console.log("new target")
 			}else if(creep.memory.target.x === creep.pos.x && creep.memory.target.y === creep.pos.y && creep.memory.target.roomName === creep.pos.roomName){
 				creep.memory.target = undefined;
-				console.log("del target")
 			}
 
  			let res = actionMove.do(creep, creep.memory.target);
@@ -34,7 +32,6 @@ let role = {
 					actionMove.do(creep, creep.room.controller);
 				}
 				ret = creep.claimController(creep.room.controller);
-				console.log(creep, creep.room.controller, ret)
 				if(ret == ERR_NOT_IN_RANGE) {
 				 	creep.moveTo(creep.room.controller);
 				}
