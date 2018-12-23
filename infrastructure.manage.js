14697f63d8406d733aa8b73a885063f7df7472cb
Moncho <morgan.moncho@gmail.com>
2018-12-23 08:09:29 +0100

    REG : construction des conteneur avant les route entre les controlleur

--- infrastructure.manage.js
+++ infrastructure.manage.js
@@ -1,4 +1,4 @@
-let Container  = require('infrastructure.container');
+let Container = require('infrastructure.container');
 let bind_structure_to_source = require('infrastructure.bind_structure_to_source');
 let bind_contoller = require('infrastructure.bind_contoller')
 let Perif = require('infrastructure.perif');
@@ -6,61 +6,63 @@ let Perif = require('infrastructure.perif');
 let infoPerf = require('info.perf');
 
 let infrastructure = {
-    manage: function(room){
-        
+    manage: function(room) {
+
         let scriptName = "infrastructure.manage";
-        infoPerf.init(scriptName,false);
-        
+        infoPerf.init(scriptName, false);
+
         let sites = room.find(FIND_CONSTRUCTION_SITES);
         let newSite = sites.length != 0;
         let sources = room.find(FIND_SOURCES);
-        
+
         // Détruire tout les site de constructions
-        if(false){
+        if (false) {
             let sites = room.find(FIND_CONSTRUCTION_SITES)
             for (let i = sites.length - 1; i >= 0; i--) {
                 let site = sites[i];
                 site.remove();
             }
         }
-        
+
         infoPerf.log(scriptName, "init variable");
 
-        // relier Les controllers par des routes
-        if(!newSite){
-            newSite = bind_contoller.build(room);
-            infoPerf.log(scriptName, "Ajouter des routes entre les controllers");
-        }
-        
         /// ajouter des conteneurs autour des sources qui n'en ont pas
         // Parcourir les sources
-        if (!newSite){
+        if (!newSite) {
             newSite = Container.build(room, sources)
             infoPerf.log(scriptName, "Ajouter des conteneurs autour des sources qui n'en ont pas");
         }
-        
+
+        // relier Les controllers par des routes
+        if (!newSite) {
+            newSite = bind_contoller.build(room);
+            infoPerf.log(scriptName, "Ajouter des routes entre les controllers");
+        }
+
         // relier Les structures et les sources par des routes
-        if(!newSite){
+        if (!newSite) {
             newSite = bind_structure_to_source.build(room, sources)
             infoPerf.log(scriptName, "Ajouter des routes entre les sources et les batiments");
         }
-        
+
         // Creer des perifieriques
-        if(!newSite){
+        if (!newSite) {
             // Creer des perifieriques autour des sources
             //newSite = Perif.build(room, sources,1);
             infoPerf.log(scriptName, "Creer les perifieriques sources 1");
-            if(!newSite){
-                newSite = Perif.build(room, sources,2);
+            if (!newSite) {
+                newSite = Perif.build(room, sources, 2);
                 infoPerf.log(scriptName, "Creer les perifieriquessources 2");
             }
-            if(!newSite){
+            if (!newSite) {
                 newSite = Perif.build(room, [room.controller], 4);
                 infoPerf.log(scriptName, "Creer les perifieriquescontroleur 4");
             }
             // Creer des perifieriques autour des storages
-            if(!newSite){
-                let containers = room.find(FIND_STRUCTURES, { filter: (struct) => struct.structureType == STRUCTURE_STORAGE });
+            if (!newSite) {
+                let containers = room.find(FIND_STRUCTURES, {
+                    filter: (struct) => struct.structureType == STRUCTURE_STORAGE
+                });
                 newSite = Perif.build(room, containers, 3);
                 infoPerf.log(scriptName, "Creer les perifieriques containers 3");
             }
