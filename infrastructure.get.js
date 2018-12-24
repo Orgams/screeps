6398f1943a455929ea2233082c732ed40d83474b
Moncho <morgan.moncho@gmail.com>
2018-12-24 18:18:55 +0100

    EVO : Creer des voies de contournement 2
    EVO : Creer des voies de contournement
    
    EVO : Creer des voies de contournement

--- infrastructure.get.js
+++ infrastructure.get.js
@@ -1,17 +1,27 @@
-let infrastructure = {
-    perif: function(target, dist, room){
-        res = [];
-        for (let offset = -dist; offset <= dist; offset++) {
-            res.push(new RoomPosition(target.pos.x + offset, target.pos.y + dist, room.name));
-            res.push(new RoomPosition(target.pos.x + offset, target.pos.y - dist, room.name));
-        }
-        let distbis = dist-1;
-        for (let offset = -distbis; offset <= distbis; offset++) {
-            res.push(new RoomPosition(target.pos.x + dist, target.pos.y + offset, room.name));
-            res.push(new RoomPosition(target.pos.x - dist, target.pos.y + offset, room.name));
-        }
-        return res;
+let perif = function(target, dist, room) {
+    res = [];
+    for (let offset = -dist; offset <= dist; offset++) {
+        res.push(new RoomPosition(target.pos.x + offset, target.pos.y + dist, room.name));
+        res.push(new RoomPosition(target.pos.x + offset, target.pos.y - dist, room.name));
     }
+    let distbis = dist - 1;
+    for (let offset = -distbis; offset <= distbis; offset++) {
+        res.push(new RoomPosition(target.pos.x + dist, target.pos.y + offset, room.name));
+        res.push(new RoomPosition(target.pos.x - dist, target.pos.y + offset, room.name));
+    }
+    return res;
 }
 
-module.exports = infrastructure;
+let ortho = function(target, room) {
+    res = [];
+    res.push(new RoomPosition(target.pos.x + 1, target.pos.y, room.name));
+    res.push(new RoomPosition(target.pos.x - 1, target.pos.y, room.name));
+    res.push(new RoomPosition(target.pos.x, target.pos.y + 1, room.name));
+    res.push(new RoomPosition(target.pos.x, target.pos.y - 1, room.name));
+    return res;
+}
+
+module.exports = {
+    perif: perif,
+    ortho: ortho
+}
--- infrastructure_road_contournement.js
+++ infrastructure_road_contournement.js
@@ -1,11 +1,21 @@
+let roadCreate = require('road.create');
+let infrastructureGet = require('infrastructure.get');
+
 let infrastructure = {
     build: function(room, sources) {
         console.log("Creer des voies de contournement")
 
         let structs = room.find(FIND_MY_STRUCTURES);
 
-        for(let struct of structs){
-            console.log(struct)
+        for (let struct of structs) {
+            let poss = infrastructureGet.ortho(struct, room)
+            for (let pos of poss) {
+                let ret = roadCreate.create(pos);
+                console.log(ret)
+                if (ret === OK) {
+                    return true;
+                }
+            }
         }
 
         return false;
