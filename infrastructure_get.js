c0bc3afb8f2723a1c78c0e33f0ecfcf555c8129a
Moncho <morgan.moncho@gmail.com>
2018-12-30 18:57:15 +0100

    EVO : infrastructure_get pos_paire

--- infrastructure_get.js
+++ infrastructure_get.js
@@ -1,32 +1,40 @@
+let info_pos = require('info_pos');
+
 let perif = function(target, dist, room) {
+    let pos = info_pos.get_pos(target);
+
     res = [];
     for (let offset = -dist; offset <= dist; offset++) {
-        res.push(new RoomPosition(target.pos.x + offset, target.pos.y + dist, room.name));
-        res.push(new RoomPosition(target.pos.x + offset, target.pos.y - dist, room.name));
+        res.push(new RoomPosition(pos.x + offset, pos.y + dist, room.name));
+        res.push(new RoomPosition(pos.x + offset, pos.y - dist, room.name));
     }
     let distbis = dist - 1;
     for (let offset = -distbis; offset <= distbis; offset++) {
-        res.push(new RoomPosition(target.pos.x + dist, target.pos.y + offset, room.name));
-        res.push(new RoomPosition(target.pos.x - dist, target.pos.y + offset, room.name));
+        res.push(new RoomPosition(pos.x + dist, pos.y + offset, room.name));
+        res.push(new RoomPosition(pos.x - dist, pos.y + offset, room.name));
     }
     return res;
 }
 
 let diago = function(target, dist, room) {
+    let pos = info_pos.get_pos(target);
+
     res = [];
-    res.push(new RoomPosition(target.pos.x + dist, target.pos.y + dist, room.name));
-    res.push(new RoomPosition(target.pos.x - dist, target.pos.y + dist, room.name));
-    res.push(new RoomPosition(target.pos.x + dist, target.pos.y - dist, room.name));
-    res.push(new RoomPosition(target.pos.x - dist, target.pos.y - dist, room.name));
+    res.push(new RoomPosition(pos.x + dist, pos.y + dist, room.name));
+    res.push(new RoomPosition(pos.x - dist, pos.y + dist, room.name));
+    res.push(new RoomPosition(pos.x + dist, pos.y - dist, room.name));
+    res.push(new RoomPosition(pos.x - dist, pos.y - dist, room.name));
     return res;
 }
 
 let ortho = function(target, dist, room) {
+    let pos = info_pos.get_pos(target);
+
     res = [];
-    res.push(new RoomPosition(target.pos.x + dist, target.pos.y, room.name));
-    res.push(new RoomPosition(target.pos.x - dist, target.pos.y, room.name));
-    res.push(new RoomPosition(target.pos.x, target.pos.y + dist, room.name));
-    res.push(new RoomPosition(target.pos.x, target.pos.y - dist, room.name));
+    res.push(new RoomPosition(pos.x + dist, pos.y, room.name));
+    res.push(new RoomPosition(pos.x - dist, pos.y, room.name));
+    res.push(new RoomPosition(pos.x, pos.y + dist, room.name));
+    res.push(new RoomPosition(pos.x, pos.y - dist, room.name));
     return res;
 }
 
