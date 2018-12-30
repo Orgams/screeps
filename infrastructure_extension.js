aded56b04b1cd8ea200f1f3f77d48c02f0cd4096
Moncho <morgan.moncho@gmail.com>
2018-12-30 14:20:03 +0100

    FIX : Create infra extension
    FIX : Create infra extension
    
    FIX : Create infra extension
    
    FIX : Create infra extension

--- infrastructure_extension.js
+++ infrastructure_extension.js
@@ -10,7 +10,7 @@ let build = function(room, sources) {
     });
     for (let container of containers) {
         let poss = infrastructure_get.diago(container, 2, room);
-        poss.concat(infrastructure_get.ortho(container, 2, room));
+        poss = poss.concat(infrastructure_get.ortho(container, 2, room));
         for (let pos of poss) {
             let ret = infrastructure_create.create(pos, STRUCTURE_EXTENSION);
             if (ret === OK) {
