f9c9a3b38b213a1c1bc602e8fef67c26d4c2687b
Moncho <morgan.moncho@gmail.com>
2018-12-31 17:35:36 +0100

    EVO : infrastructure_create redétruire les routes

--- infrastructure_create.js
+++ infrastructure_create.js
@@ -12,19 +12,21 @@ let create = function(pos, struct_type) {
 	// Vérifier si il n'y a pas déjà une structure
 	let structs = pos.lookFor(LOOK_STRUCTURES);
 	let structs_without_road = _.filter(structs, (struct) => struct.structureType !== STRUCTURE_ROAD)
-	if(structs_without_road.length > 0){
+	if (structs_without_road.length > 0) {
 		return false;
 	}
 
-	// Détruire la route sous la structure si il y en a une
-	// let roads = _.filter(structs, (struct) => struct.structureType === STRUCTURE_ROAD)
-	// if(structs.length > 0){
-	// 	roads[0].destroy();
-	// }
-	
 	// Construire la structure
 	let ret = pos.createConstructionSite(struct_type);
 	//infoPerf.simpleLog(scriptName, "create road : " + pos.x +" "+ pos.y +" "+ terrain+" "+ terrain != "plain" +" "+terrain != "swamp"+" "+ ret)
+
+	// Détruire la route sous la structure si il y en a une et si le site de construction est placé
+	if (ret === OK) {
+		let roads = _.filter(structs, (struct) => struct.structureType === STRUCTURE_ROAD)
+		if (structs.length > 0) {
+			roads[0].destroy();
+		}
+	}
 	return ret;
 }
 
