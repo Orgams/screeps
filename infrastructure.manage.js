2b17c2c56aa6e6e7df640f6927167407ec957935
Moncho <morgan.moncho@gmail.com>
2018-12-31 13:59:39 +0100

    EVO : infrastructure_create ne pas creer de route sous les batimant et détruire une route à l'emplacement d'un future batiment

--- infrastructure.manage.js
+++ infrastructure.manage.js
@@ -5,7 +5,7 @@ let infrastructure = {
     manage: function(room) {
 
         let scriptName = "infrastructure.manage";
-        infoPerf.init(scriptName, false);
+        infoPerf.init(scriptName, true);
 
         let sites = room.find(FIND_CONSTRUCTION_SITES);
         let newSite = sites.length != 0;
