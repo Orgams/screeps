c97c349a92821705cb47dd53be2ba17ad0aafd44
Moncho <morgan.moncho@gmail.com>
2018-12-31 18:09:39 +0100

    EVO : Action build passer en mode global si le site à coinstruir est dans un autre salle

--- action.build.js
+++ action.build.js
@@ -8,19 +8,19 @@ let action = {
 		let target = creep.pos.findClosestByRange(targets);
 
 		// Chercher un site dans les autres salles si besoin
-		if(target == null){
+		if(target === null){
 			targets = Game.constructionSites;
 			target = targets[Object.keys(targets)[0]];
 
 			// Passer le creep en mode global si la cible est dans une autre piece
-			if(target == null){
+			if(target !== null){
 				memoire.set("range", "global", creep, 10);
 				memoire.get("range", creep);
 			}
 		}
 
 		// Tenter de construire le site s'il existe
-		if(target != null){
+		if(target !== null){
 			if(creep.build(target) == ERR_NOT_IN_RANGE) {
 				actionMove.do(creep, target);
 			}
