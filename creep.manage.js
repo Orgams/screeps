270e7d5bcc982826829533dbf96e7cc6d454a553
Moncho <morgan.moncho@gmail.com>
2019-01-01 14:14:15 +0100

    EVO : Action stockage multi room

--- creep.manage.js
+++ creep.manage.js
@@ -81,7 +81,7 @@ let fonc_manage_creep = function(room) {
 
     console.log("avant", configs)
     // Eliminer les configuration qui sont arriver à leur max de population
-    configs = configs.filter((config) => console.log(config) || config.maxOk());
+    configs = configs.filter((config) => console.log(config.maxOk()) || config.maxOk());
     infoPerf.log(scriptName, "Delete conf in max");
     console.log("après", configs)
 
