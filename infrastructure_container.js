8b0d7f5b210c4763b3472d22d6c4138f16641c3d
Moncho <morgan.moncho@gmail.com>
2019-01-01 18:34:26 +0100

    EVO : Action stockage multi room

--- infrastructure_container.js
+++ infrastructure_container.js
@@ -11,6 +11,7 @@ let infrastructure = {
 
         for (let i = sources.length - 1; i >= 0; i--) {
             let source = sources[i];
+            infoPerf.log(scriptName, "debut source ", source);
 
             // recuperer les sites de construction de containers autour de la source
             let constrs = source.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 1, {
@@ -21,6 +22,7 @@ let infrastructure = {
                     return false;
                 }
             });
+            infoPerf.log(scriptName, "recuperer les sites de construction de containers autour de la source");
 
             // recuperer les containers autour de la source
             let structs = source.pos.findInRange(FIND_STRUCTURES, 1, {
@@ -31,15 +33,17 @@ let infrastructure = {
                     return false;
                 }
             });
+            infoPerf.log(scriptName, "recuperer les containers autour de la source");
 
-            //si il n'y a ni site de construction ni structure conteneur, l'ajouter
+            // Ajouter le site de construction s'il n'existe pas et s'il n'y a pas de conteneur
             if (constrs.length === 0 && structs.length === 0) {
                 let pos = infrastructure_get.pos_on_path(source, room.controller, 1, room.name);
                 infrastructure_create.create(pos, STRUCTURE_CONTAINER);
                 return true;
             }
+            infoPerf.log(scriptName, "Ajouter le site de construction s'il n'existe pas et s'il n'y a pas de conteneur");
 
-            infoPerf.log(scriptName, "fin source ", source);
+            
         }
         infoPerf.finish(scriptName);
         return false;
