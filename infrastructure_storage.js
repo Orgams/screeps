9b6a5124e539154a5439cfa3a0865f0062833e4b
Moncho <morgan.moncho@gmail.com>
2019-01-02 14:39:22 +0100

    EVO : Affichage détails creeps

--- infrastructure_storage.js
+++ infrastructure_storage.js
@@ -3,6 +3,7 @@ let info_room = require('info.room');
 
 let build = function(room, sources) {
     let pos = info_room.get_pos_center(room.name);
+    console.log("pos : ", pos);
     return infrastructure_create.create(pos, STRUCTURE_STORAGE);
 }
 
