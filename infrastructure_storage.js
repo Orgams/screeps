5801b78176e4e5b1f2c7317d87ba5256431edd1b
Moncho <morgan.moncho@gmail.com>
2019-01-02 14:43:13 +0100

    FIX : infrastructure_storage

--- infrastructure_storage.js
+++ infrastructure_storage.js
@@ -2,11 +2,15 @@ let infrastructure_create = require('infrastructure_create');
 let info_room = require('info.room');
 
 let build = function(room, sources) {
-    let pos = info_room.get_pos_center(room.name);
-    console.log("pos : ", pos);
-    return infrastructure_create.create(pos, STRUCTURE_STORAGE);
+	let pos = info_room.get_pos_center(room.name);
+	console.log("pos : ", pos);
+	let ret = infrastructure_create.create(pos, STRUCTURE_STORAGE);
+	if (ret === OK) {
+		return true;
+	}
+	return ret
 }
 
 module.exports = {
-    build: build
+	build: build
 }
