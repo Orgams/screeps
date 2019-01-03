1f63e6f4626313b4a6f92cdce9d004a80bcfcc2a
Moncho <morgan.moncho@gmail.com>
2019-01-03 13:07:52 +0100

    FIX : infrastructure_perif

--- infrastructure_road_contournement.js
+++ infrastructure_road_contournement.js
@@ -2,11 +2,12 @@ let infrastructure_create = require('infrastructure_create');
 let infrastructure_get = require('infrastructure_get');
 
 let infrastructure = {
-    build: function(room, sources) {
+    build: function(room) {
         let structs = room.find(FIND_MY_STRUCTURES);
 
         for (let struct of structs) {
             let poss = infrastructure_get.ortho(struct, 1, room)
+            console.log (struct, poss)
             for (let pos of poss) {
                 let ret = infrastructure_create.create(pos, STRUCTURE_ROAD);
                 if (ret === OK) {
