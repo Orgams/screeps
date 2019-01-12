014c352ce4ad4411a0a22cf37d6ee6600156379c
Moncho <morgan.moncho@gmail.com>
2019-01-12 09:36:26 +0100

    REF : memoire

--- memoire.js
+++ memoire.js
@@ -2,52 +2,54 @@ let info_perf = require('info_perf');
 
 let scriptName = "memoire"
 
-let memoire = {
-	set: function(key, valeur, target, ttl) {
-		let keyParts = key.split(".");
-		let keyCur = keyParts[0];
-		keyParts.shift(1);
 
-		// Initialiser l'objet où il faut ajouter une donnée en mémoire
-		let obj = null;
-		if (target === undefined) {
-			obj = Memory;
-		} else {
-			obj = target.memory;
-		}
 
-		// Appel récursif pour creer les objet et mettre la valeur à la fin
-		setRecur(obj, keyCur, keyParts, valeur);
+let set = function(key, valeur, target, ttl) {
+	let keyParts = key.split(".");
+	let keyCur = keyParts[0];
+	keyParts.shift(1);
 
-		// Ajouter une valeur ttl si demandé
-		if (ttl !== undefined) {
-			let keyParts = (key + "ttl").split(".");
-			let keyCur = keyParts[0];
-			keyParts.shift(1);
-
-			setRecur(obj, keyCur, keyParts, Game.time + ttl);
-		}
-	},
-	get: function(key, target) {
-		let keyParts = key.split(".");
-		let keyCur = keyParts[0];
-		keyParts.shift(1);
-
-		// Initialiser l'objet où il faut ajouter une donnée en mémoire
-		let obj = null;
-		if (target === undefined) {
-			obj = Memory;
-		} else {
-			obj = target.memory;
-		}
-
-		// Appel récursif pour creer les objet et mettre la valeur à la fin
-		return getRecur(obj, keyCur, keyParts);
+	// Initialiser l'objet où il faut ajouter une donnée en mémoire
+	let obj = null;
+	if (target === undefined) {
+		obj = Memory;
+	} else {
+		obj = target.memory;
 	}
-};
+
+	// Appel récursif pour creer les objet et mettre la valeur à la fin
+	setRecur(obj, keyCur, keyParts, valeur);
+
+	// Ajouter une valeur ttl si demandé
+	if (ttl !== undefined) {
+		let keyParts = (key + "ttl").split(".");
+		let keyCur = keyParts[0];
+		keyParts.shift(1);
+
+		setRecur(obj, keyCur, keyParts, Game.time + ttl);
+	}
+}
+
+let get = function(key, target) {
+	let keyParts = key.split(".");
+	let keyCur = keyParts[0];
+	keyParts.shift(1);
+
+	// Initialiser l'objet où il faut ajouter une donnée en mémoire
+	let obj = null;
+	if (target === undefined) {
+		obj = Memory;
+	} else {
+		obj = target.memory;
+	}
+
+	// Appel récursif pour creer les objet et mettre la valeur à la fin
+	return getRecur(obj, keyCur, keyParts);
+}
+
 
 let getRecur = function(obj, keyCur, keyParts) {
-	if (obj === undefined){
+	if (obj === undefined) {
 		return undefined;
 	}
 	if (keyParts.length > 0) {
@@ -56,7 +58,7 @@ let getRecur = function(obj, keyCur, keyParts) {
 		keyParts.shift(1);
 		return getRecur(obj, keyCur, keyParts);
 	} else {
-		if(obj[keyCur+"ttl"] !== undefined && obj[keyCur+"ttl"] < Game.time){
+		if (obj[keyCur + "ttl"] !== undefined && obj[keyCur + "ttl"] < Game.time) {
 			//info_perf.simpleLog(scriptName, "data out to date");
 			return undefined
 		}
@@ -80,4 +82,7 @@ let setRecur = function(obj, keyCur, keyParts, valeur) {
 	}
 }
 
-module.exports = memoire;
+module.exports = {
+	set: set,
+	get: get
+};
