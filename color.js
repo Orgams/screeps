3ae9b0c0252a2b6aed076d73ce87b9f098d58a9c
Moncho <morgan.moncho@gmail.com>
2019-04-13 18:52:26 +0200

    EVO : gestion de la couleur des creeps

--- color.js
+++ color.js
@@ -1,20 +1,13 @@
 let color = {
     get: function(roleCreep){
         switch (roleCreep) {
-            case "transferer" :
-                return "#00ff00";
-            case "janitor" :
-                return "#00ffff";
-            case "miner" :
-                return "#ff00ff";
-            case "builder" :
-                return "#ff0000";
-            case "upgrader" :
-                return "#0000ff";
-            case "repairer" :
-                return "#ff9900";
-            case "claimer" :
-                return "#ffff00";
+            case 'transferer' : console.log("#00ff00");
+            case 'janitor' : console.log("#00ffff");
+            case 'miner' : console.log("#ff00ff");
+            case 'builder' : console.log("#ff0000");
+            case 'upgrader' : console.log("#0000ff");
+            case 'repairer' : console.log("#ff9900");
+            case 'claimer' : console.log("#ffff00");
         }
     }
 };
