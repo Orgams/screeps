e3aed550eba03f9cf14ff7a09980ceb0211d8770
Moncho <morgan.moncho@gmail.com>
2019-04-13 18:31:56 +0200

    FIX : role undefine

---
+++ color.js
@@ -0,0 +1,17 @@
+let memoire = require('memoire');
+
+let color = {
+    getColor : function(roleCreep){
+        switch (roleCreep) {
+            case 'transferer' : return "#00ff00";
+            case 'janitor' : return "#00ffff";
+            case 'miner' : return "#ff00ff";
+            case 'builder' : return "#ff0000";
+            case 'upgrader' : return "#0000ff";
+            case 'repairer' : return "#ff9900";
+            case 'claimer' : return "#ffff00";
+        }
+    }
+};
+
+module.exports = color;
